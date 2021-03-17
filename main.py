7f41a659939b116697d7389cf01018a6b9c99d6a
vladyslav.tkachuk <vladyslav.tkachuk99@gmail.com>
2021-03-17 20:44:36 +0200

    commit changes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import config
 import telebot
 import requests
-from bs4 import BeautifulSoup as BS             # import necessary libraries
+from bs4 import BeautifulSoup as BS    # import necessary libraries
+from telebot import types
 
 r = requests.get('https://sinoptik.ua/погода-киев')
 html = BS(r.content, 'html.parser')
@@ -15,8 +16,12 @@ for el in html.select('#content'):
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, "Привет, погода на сегодня:\n" +
-       t_min + "," + t_max + '\n' + text)
+    bot.send_message(message.chat.id,"Привет! Погода в Киеве на сегодня:\n" +
+    t_min + "," + t_max + '\n' + text)
+
+@bot.message_handler(commands=['thanks'])
+def main(message):
+    bot.send_message(message.chat.id, "Спасибо, что воспользовались ботом!\nХорошего дня!")
 
 if __name__ == '__main__':               # bot.polling starts only in case, if it's the main file
     bot.polling(none_stop=True)          # if someone import this file bot.polling won't work
